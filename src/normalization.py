143666a49c01e25519eca46ad3caba2e518df188
serddmitry <dmitry.serdiuk@gmail.com>
2010-11-03 20:51:21 +0200

    normalization works (casting to float before division)

--- src/normalization.py
+++ src/normalization.py
@@ -13,9 +13,10 @@ class Linear:
     [0, 0.25, 0.5, 0.75, 1], so we could calculate scores using different
     parameters.
     '''
+    
     def init(self, data_set):
         self.xmin = min(data_set)
         self.xmax = max(data_set)
     
     def normalize(self, value):
-        return (value - self.xmin) / (self.xmax - self.xmin)
+        return (value - self.xmin) / float(self.xmax - self.xmin)
